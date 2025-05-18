288047855d95cc3b5bdf5c4e2ff78ce00532cfaf
Busd Digital Reserve <busddigitalreserve@gmail.com>
2025-05-18 17:57:06 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "swap-bot",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "dotenv": "^16.0.3",
+    "ethers": "^6.6.2"
+  }
+}
